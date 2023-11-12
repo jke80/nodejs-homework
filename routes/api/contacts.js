6c9023eab7fb71cf8349447444a4055000a78dff
yevhen kolomoitsev <kolomoycev81@gmail.com>
2023-11-12 15:18:44 +0200

    test commit

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,24 +2,34 @@ const express = require('express')
 
 const router = express.Router()
 
+const {listContacts, getContactById} = require('../../models/contacts');
+
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const data = await listContacts();
+  res.json(data);
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const {contactId} = req.params;
+  const data = await getContactById(contactId); 
+  res.json(data);
 })
 
 router.post('/', async (req, res, next) => {
+
+  
   res.json({ message: 'template message' })
+  console.log('post/');
 })
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
+  console.log('del/');
 })
 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
+  console.log('put/');
 })
 
 module.exports = router
