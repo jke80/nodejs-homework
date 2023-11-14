b88175574980b3b43462a94c765c4127f3af2b06
Evhen Kolomoitsev <127289152+jke80@users.noreply.github.com>
2023-11-15 00:43:58 +0200

    Update updateById.js
    delete comments

--- controllers/contacts/updateById.js
+++ controllers/contacts/updateById.js
@@ -3,12 +3,6 @@ const {updateContact} = require("../../models/contacts");
 
 const updateById = async (req, res) => {
     const { contactId } = req.params;
-    // console.log("------",Object.keys(req.body).length);
-    // if(!Object.keys(req.body).length){
-    //     res.status(400).json({
-    //     message:"missing fields"
-    //   })
-    // }
     const data = await updateContact(contactId, req.body);
     if(!data){
       throw NotFound(`Not Found`);
