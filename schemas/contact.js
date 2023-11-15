080ebb5b1b7b0d38ec6f4bc6526739a0a4863c22
Evhenii <kolomoycev81@gmail.com>
2023-11-16 00:35:47 +0200

    fix: custom messages in Joi schema

--- schemas/contact.js
+++ schemas/contact.js
@@ -1,10 +1,23 @@
 const Joi = require("joi");
 
-const contactSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().required(),
-    phone: Joi.string().required()
-})
-
+const contactSchema = Joi.object()
+  .min(1)
+  .messages({
+    "object.min": "missing fields",
+  })
+  .when(Joi.object().min(1), {
+    then: Joi.object({
+      name: Joi.string()
+        .required()
+        .messages({ "any.required": "missing required name field" }),
+      email: Joi.string()
+        .email()
+        .required()
+        .messages({ "any.required": "missing required email field" }),
+      phone: Joi.string()
+        .required()
+        .messages({ "any.required": "missing required phone field" }),
+    }),
+  });
 
 module.exports = contactSchema;
