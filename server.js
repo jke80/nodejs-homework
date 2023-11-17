bc5b8821e45561d0cb5c38166721a07f75edd6c5
Evhenii <kolomoycev81@gmail.com>
2023-11-17 18:03:18 +0200

    connect to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose
+  .connect(
+    "mongodb+srv://Evgen:1DpaW14lvWotFHPy@cluster0.foobhck.mongodb.net/phonebook?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(3000, () => console.log("connect"));
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
