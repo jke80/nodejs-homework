98d0e3cda0891617a2d569ba9e9cb0f977adc02d
Evhenii <kolomoycev81@gmail.com>
2023-11-19 14:48:38 +0200

    fix:  add .env.example  schema edit

--- models/contact.js
+++ models/contact.js
@@ -36,8 +36,13 @@ const contactSchema = Joi.object()
       name: Joi.string()
         .required()
         .messages({ "any.required": "missing required name field" }),
-      email: Joi.string().email(),
-      phone: Joi.string(),
+      email: Joi.string()
+        .email()
+        .required()
+        .messages({ "any.required": "missing required email field" }),
+      phone: Joi.string()
+        .required()
+        .messages({ "any.required": "missing required phone field" }),
       favorite: Joi.boolean(),
     }),
   });
