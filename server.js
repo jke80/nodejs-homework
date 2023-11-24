8ea886acda8240d390c64720e6c7e578d7ae3dde
Evhenii <kolomoycev81@gmail.com>
2023-11-24 08:05:52 +0200

    add: port in env, gravatar in register

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000, () => console.log("Database connection successful"));
+    app.listen(PORT || 3000, () =>
+      console.log("Database connection successful")
+    );
   })
   .catch((err) => {
     console.log(err.message);
